1202f36eacfb8239ec1134440c363f781c10c6fc
Phil Ross <phil.ross@gmail.com>
2005-11-26 19:24:57 +0000

    TimezonePeriodList tests and implementation.

--- tzinfo/lib/tzinfo/timezone_period_list.rb
+++ tzinfo/lib/tzinfo/timezone_period_list.rb
@@ -9,7 +9,23 @@ module TZInfo
   class TimezonePeriodList
     # Initializes a new TimezonePeriodList
     def initialize
-      raise 'Not Implemented'
+      @list = nil
+      @last_year = nil
+      @last_month = nil
+      @start_year = nil
+      @start_month = nil
+      @unbounded_start = nil
+    end
+    
+    # One unbounded start period can be defined. This must be set before any
+    # calls to add.
+    #
+    # ArgumentError will be thrown if add or add_unbounded_start has already
+    # been called.
+    def add_unbounded_start(&block)
+      raise ArgumentError, 'add_unbounded_start has already been called' if !@unbounded_start.nil?
+      raise ArgumentError, 'add has already been called' if !@list.nil?
+      @unbounded_start = block
     end
     
     # Adds a new TimezonePeriod to the list. utc_start_year and utc_start_month
@@ -19,21 +35,169 @@ module TZInfo
     #
     # If the start year and month are less than the previously encountered
     # year and month ArgumentError will be thrown.
-    def add(utc_start_year, utc_start_month, &block)
-      raise 'Not Implemented'
+    #
+    # Blocks must be added in order of increasing UTC start date.
+    def add(utc_start_year, utc_start_month, &block)                 
+      if @list.nil?
+        @list = [block]
+        @start_year = utc_start_year
+        @start_month = utc_start_month
+      else
+        if utc_start_year < @last_year || (utc_start_year == @last_year && utc_start_month < @last_month)
+          raise ArgumentError, 'Calls to add must be in increasing start date order'
+        end
+                         
+        index = index_for_utc(utc_start_year, utc_start_month)
+        if @list[index].nil?
+          @list[index] = block
+        elsif @list[index].is_a?(Array)
+          @list[index] << block
+        else
+          @list[index] = [@list[index], block]
+        end
+      end
+        
+      @last_year = utc_start_year
+      @last_month = utc_start_month        
     end
     
-    # Returns the period for the given utc time. The earliest and latest periods
-    # are considered to be unbounded.
-    # If there are no periods, PeriodNotFound is thrown.
+    # Returns the period for the given utc time. 
+    # If no period is found for the given time, PeriodNotFound is thrown.
     def period_for_utc(utc)
-      raise 'Not Implemented'
+      raise PeriodNotFound, 'No periods defined' if @list.nil?
+      index = index_for_utc(utc.year, utc.mon)
+      index = 0 if index < 0
+      index = @list.length - 1 if index >= @list.length
+      
+      result = nil
+      index.downto(0) {|i|
+        contents = get_contents(i)
+        
+        if contents.is_a?(Array)
+          (contents.length - 1).downto(0) {|j|
+            if contents[j].valid_for_utc?(utc)
+              result = contents[j]
+              break
+            end
+          }
+          break unless result.nil?
+        elsif !contents.nil? && contents.valid_for_utc?(utc)
+          result = contents
+          break        
+        end
+      }
+      
+      if result.nil?
+        unbounded_start = get_unbounded_start
+        if !unbounded_start.nil? && unbounded_start.valid_for_utc?(utc)          
+          unbounded_start
+        else
+          raise PeriodNotFound, "No period found for utc: #{utc}"
+        end
+      else
+        result
+      end
     end
     
-    # Returns the set of periods for the given local time as an array. The 
-    # earliest and latest periods are considered to be unbounded.
+    # Returns the set of periods for the given local time as an array.
+    # If no period is found for the given time, PeriodNotFound is thrown.
+    # Results returned are ordered by increasing UTC start date.    
     def periods_for_local(local)
-      raise 'Not Implemented'
+      raise PeriodNotFound, 'No periods defined' if @list.nil?
+      
+      # add one to the index because the periods are indexed by UTC start
+      # and the period we are looking for could therefore be in the next
+      # half of the year
+      index = index_for_utc(local.year, local.mon) + 1
+      index = 0 if index < 0
+      index = @list.length - 1 if index >= @list.length
+      
+      result = []
+      
+      # found_first is used to avoid searching back to far
+      # the search will be limited at the next half year block after discovering
+      # the first match
+      found_first = nil
+      
+      index.downto(0) {|i|
+        contents = get_contents(i)
+        
+        if contents.is_a?(Array)
+          (contents.length - 1).downto(0) {|j|
+            if contents[j].valid_for_local?(local)
+              result << contents[j]
+              found_first = i if found_first.nil?
+            end
+          }
+          
+          if !found_first.nil? && i < found_first
+            # searched far enough
+            break
+          end
+        elsif !contents.nil? && contents.valid_for_local?(local)
+          result << contents                  
+        end
+      }
+                 
+      if result.empty? || found_first == 0
+        unbounded_start = get_unbounded_start
+        result << unbounded_start if !unbounded_start.nil? && unbounded_start.valid_for_local?(local)
+      end
+      
+      if result.empty?        
+        raise PeriodNotFound, "No period found for utc: #{local}"
+      else
+        result.reverse!
+        result
+      end
     end
+    
+    private
+      # Returns the index into the @list array for a given utc year and month.
+      def index_for_utc(year, month)
+        index = (year - @start_year) * 2
+        index += 1 if month > 6
+        index -= 1 if @start_month > 6
+        index
+      end
+      
+      # Returns the contents of any 6 month period, exapanding any blocks to
+      # TimezonePeriods first.
+      def get_contents(index)
+        contents = @list[index]
+        if contents.nil?
+          nil
+        elsif contents.is_a?(TimezonePeriod)
+          contents
+        elsif contents.is_a?(Array)
+          if contents.first.is_a?(TimezonePeriod)
+            contents
+          else
+            contents = contents.collect {|proc|
+              proc.call
+            }
+            @list[index] = contents
+            contents       
+          end
+        elsif contents.is_a?(Proc)
+          contents = contents.call
+          @list[index] = contents
+          contents
+        end                
+      end
+      
+      # Expands unbounded_start if it is defined.
+      def get_unbounded_start
+        contents = @unbounded_start
+        if contents.nil?
+          nil
+        elsif contents.is_a?(TimezonePeriod)
+          contents
+        else
+          contents = contents.call
+          @unbounded_start = contents
+          contents
+        end
+      end
   end
 end
--- tzinfo/test/tc_timezone_period_list.rb
+++ tzinfo/test/tc_timezone_period_list.rb
@@ -23,29 +23,111 @@ class TCTimezonePeriod < Test::Unit::TestCase
     assert_raise(ArgumentError) {      
       list.add(2005, 2) { TimezonePeriod.new(DateTime.new(2005,2,1,1,0,0), DateTime.new(2005,3,1,1,0,0), 0, 0, :TEST) }      
     }
+    
+    assert_raise(ArgumentError) {      
+      list.add(2004, 12) { TimezonePeriod.new(DateTime.new(2004,12,1,1,0,0), DateTime.new(2005,1,1,1,0,0), 0, 0, :TEST) }      
+    }
+    
+    assert_raise(ArgumentError) {
+      list.add_unbounded_start { TimezonePeriod.new(nil, DateTime.new(2005,1,1,1,0,0), 0, 0, :TEST) }
+    }
+  end
+  
+  def test_add_incorrect_order_unbounded
+    list = TimezonePeriodList.new    
+    list.add_unbounded_start { TimezonePeriod.new(nil, DateTime.new(2005,1,1,1,0,0), 0, 0, :TEST) }
+    
+    assert_raise(ArgumentError) {
+      list.add_unbounded_start { TimezonePeriod.new(nil, DateTime.new(2005,1,1,1,0,0), 0, 0, :TEST) }
+    }
   end
   
   def test_period_for_utc
     list = TimezonePeriodList.new
-    list.add(2000, 4)  { TimezonePeriod.new(DateTime.new(2000,4,2,7,0,0),DateTime.new(2000,10,29,6,0,0),-18000,3600,:'EDT') }
-    list.add(2000, 10) { TimezonePeriod.new(DateTime.new(2000,10,29,6,0,0),DateTime.new(2001,4,1,7,0,0),-18000,0,:'EST') }    
+    list.add(2000, 4)  { TimezonePeriod.new(DateTime.new(2000,4,1,1,0,0),DateTime.new(2000,10,1,1,0,0),-18000,3600,:'TESTD') }
+    list.add(2000, 10) { TimezonePeriod.new(DateTime.new(2000,10,1,1,0,0),DateTime.new(2001,3,1,1,0,0),-18000,0,:'TESTS') }    
+    list.add(2001, 3)  { TimezonePeriod.new(DateTime.new(2001,3,1,1,0,0),DateTime.new(2001,4,1,1,0,0),-18000,3600,:'TESTD') }
+    list.add(2001, 4)  { TimezonePeriod.new(DateTime.new(2001,4,1,1,0,0),DateTime.new(2001,10,1,1,0,0),-21600,3600,:'TESTD') }
+    list.add(2001, 10)  { TimezonePeriod.new(DateTime.new(2001,10,1,1,0,0),DateTime.new(2002,10,1,1,0,0),-18000,0,:'TESTS') }
+    list.add(2002, 10)  { TimezonePeriod.new(DateTime.new(2002,10,1,1,0,0),DateTime.new(2003,2,1,1,0,0),-18000,0,:'TESTS') }
     
-    assert_period_equal(list.period_for_utc(DateTime.new(1999,12,1,0,0,0)),
-      TimezonePeriod.new(DateTime.new(2000,4,2,7,0,0),DateTime.new(2000,10,29,6,0,0),-18000,3600,:'EDT'))
-    assert_period_equal(list.period_for_utc(DateTime.new(2000,3,1,0,0,0)),
-      TimezonePeriod.new(DateTime.new(2000,4,2,7,0,0),DateTime.new(2000,10,29,6,0,0),-18000,3600,:'EDT'))
-    assert_period_equal(list.period_for_utc(DateTime.new(2000,4,2,7,0,0)),
-      TimezonePeriod.new(DateTime.new(2000,4,2,7,0,0),DateTime.new(2000,10,29,6,0,0),-18000,3600,:'EDT'))
-    assert_period_equal(list.period_for_utc(DateTime.new(2000,10,29,5,59,59)),
-      TimezonePeriod.new(DateTime.new(2000,4,2,7,0,0),DateTime.new(2000,10,29,6,0,0),-18000,3600,:'EDT'))
-    assert_period_equal(list.period_for_utc(DateTime.new(2000,10,29,6,0,0)),
-      TimezonePeriod.new(DateTime.new(2000,10,29,6,0,0),DateTime.new(2001,4,1,7,0,0),-18000,0,:'EST'))
-    assert_period_equal(list.period_for_utc(DateTime.new(2000,12,1,0,0,0)),
-      TimezonePeriod.new(DateTime.new(2000,10,29,6,0,0),DateTime.new(2001,4,1,7,0,0),-18000,0,:'EST'))      
+    # TimezonePeriodList expands out blocks and caches the results
+    # run twice to ensure this is being done correctly
+    2.times {|i|    
+      assert_raise(PeriodNotFound) {
+        list.period_for_utc(DateTime.new(1999,12,1,0,0,0))
+      }
+      assert_raise(PeriodNotFound) {
+        list.period_for_utc(DateTime.new(2000,4,1,0,59,59))
+      }      
+      assert_period_equal(TimezonePeriod.new(DateTime.new(2000,4,1,1,0,0),DateTime.new(2000,10,1,1,0,0),-18000,3600,:'TESTD'),
+        list.period_for_utc(DateTime.new(2000,4,1,1,0,0)))
+      assert_period_equal(TimezonePeriod.new(DateTime.new(2000,4,1,1,0,0),DateTime.new(2000,10,1,1,0,0),-18000,3600,:'TESTD'),
+        list.period_for_utc(DateTime.new(2000,10,1,0,59,59)))
+      assert_period_equal(TimezonePeriod.new(DateTime.new(2000,10,1,1,0,0),DateTime.new(2001,3,1,1,0,0),-18000,0,:'TESTS'),
+        list.period_for_utc(DateTime.new(2000,10,1,1,0,0)))
+      assert_period_equal(TimezonePeriod.new(DateTime.new(2000,10,1,1,0,0),DateTime.new(2001,3,1,1,0,0),-18000,0,:'TESTS'),
+        list.period_for_utc(DateTime.new(2001,3,1,0,59,59)))
+      assert_period_equal(TimezonePeriod.new(DateTime.new(2001,3,1,1,0,0),DateTime.new(2001,4,1,1,0,0),-18000,3600,:'TESTD'),
+        list.period_for_utc(DateTime.new(2001,3,1,1,0,0)))
+      assert_period_equal(TimezonePeriod.new(DateTime.new(2001,3,1,1,0,0),DateTime.new(2001,4,1,1,0,0),-18000,3600,:'TESTD'),
+        list.period_for_utc(DateTime.new(2001,4,1,0,59,59)))
+      assert_period_equal(TimezonePeriod.new(DateTime.new(2001,4,1,1,0,0),DateTime.new(2001,10,1,1,0,0),-21600,3600,:'TESTD'),
+        list.period_for_utc(DateTime.new(2001,4,1,1,0,0)))
+      assert_period_equal(TimezonePeriod.new(DateTime.new(2001,4,1,1,0,0),DateTime.new(2001,10,1,1,0,0),-21600,3600,:'TESTD'),
+        list.period_for_utc(DateTime.new(2001,10,1,0,59,59)))
+      assert_period_equal(TimezonePeriod.new(DateTime.new(2001,10,1,1,0,0),DateTime.new(2002,10,1,1,0,0),-18000,0,:'TESTS'),
+        list.period_for_utc(DateTime.new(2001,10,1,1,0,0)))
+      assert_period_equal(TimezonePeriod.new(DateTime.new(2001,10,1,1,0,0),DateTime.new(2002,10,1,1,0,0),-18000,0,:'TESTS'),
+        list.period_for_utc(DateTime.new(2002,2,1,1,0,0)))
+      assert_period_equal(TimezonePeriod.new(DateTime.new(2001,10,1,1,0,0),DateTime.new(2002,10,1,1,0,0),-18000,0,:'TESTS'),
+        list.period_for_utc(DateTime.new(2002,10,1,0,59,59)))
+      assert_period_equal(TimezonePeriod.new(DateTime.new(2002,10,1,1,0,0),DateTime.new(2003,2,1,1,0,0),-18000,0,:'TESTS'),
+        list.period_for_utc(DateTime.new(2002,10,1,1,0,0)))
+      assert_period_equal(TimezonePeriod.new(DateTime.new(2002,10,1,1,0,0),DateTime.new(2003,2,1,1,0,0),-18000,0,:'TESTS'),
+        list.period_for_utc(DateTime.new(2003,2,1,0,59,59)))
+      
+      assert_raise(PeriodNotFound) {
+        list.period_for_utc(DateTime.new(2003,2,1,1,0,0))
+      }
+      assert_raise(PeriodNotFound) {
+        list.period_for_utc(DateTime.new(2004,1,1,1,0,0))
+      }
+    }
   end
   
-  def test_period_for_utc_dense
-    # test with more than one start in a six month period
+  def test_period_for_utc_unbounded
+    list = TimezonePeriodList.new
+    list.add_unbounded_start  { TimezonePeriod.new(nil,DateTime.new(2000,10,1,1,0,0),-18000,3600,:'TESTD') }
+    list.add(2000, 10) { TimezonePeriod.new(DateTime.new(2000,10,1,1,0,0),DateTime.new(2001,3,1,1,0,0),-18000,0,:'TESTS') }    
+    list.add(2001, 3)  { TimezonePeriod.new(DateTime.new(2001,3,1,1,0,0),nil,-18000,3600,:'TESTD') }
+        
+    assert_period_equal(TimezonePeriod.new(nil,DateTime.new(2000,10,1,1,0,0),-18000,3600,:'TESTD'),
+      list.period_for_utc(DateTime.new(1999,12,1,1,0,0)))
+    assert_period_equal(TimezonePeriod.new(nil,DateTime.new(2000,10,1,1,0,0),-18000,3600,:'TESTD'),
+      list.period_for_utc(DateTime.new(2000,10,1,0,59,59)))
+    assert_period_equal(TimezonePeriod.new(DateTime.new(2000,10,1,1,0,0),DateTime.new(2001,3,1,1,0,0),-18000,0,:'TESTS'),
+      list.period_for_utc(DateTime.new(2000,10,1,1,0,0)))
+    assert_period_equal(TimezonePeriod.new(DateTime.new(2000,10,1,1,0,0),DateTime.new(2001,3,1,1,0,0),-18000,0,:'TESTS'),
+      list.period_for_utc(DateTime.new(2001,3,1,0,59,59)))
+    assert_period_equal(TimezonePeriod.new(DateTime.new(2001,3,1,1,0,0),nil,-18000,3600,:'TESTD'),
+      list.period_for_utc(DateTime.new(2001,3,1,1,0,0)))
+    assert_period_equal(TimezonePeriod.new(DateTime.new(2001,3,1,1,0,0),nil,-18000,3600,:'TESTD'),
+      list.period_for_utc(DateTime.new(2002,1,1,1,0,0)))
+  end
+  
+  def test_period_for_utc_gap
+    list = TimezonePeriodList.new
+    list.add(2000, 4)  { TimezonePeriod.new(DateTime.new(2000,4,1,1,0,0),DateTime.new(2000,5,1,1,0,0),-18000,3600,:'TESTD') }
+    list.add(2000, 10) { TimezonePeriod.new(DateTime.new(2000,10,1,1,0,0),DateTime.new(2001,3,1,1,0,0),-18000,0,:'TESTS') }
+    
+    assert_raise(PeriodNotFound) {
+      list.period_for_utc(DateTime.new(2005,5,1,1,0,0))
+    }
+    
+    assert_raise(PeriodNotFound) {
+      list.period_for_utc(DateTime.new(2005,10,1,0,59,59))
+    }
   end
   
   def test_period_for_utc_empty
@@ -56,10 +138,134 @@ class TCTimezonePeriod < Test::Unit::TestCase
   end
   
   def test_periods_for_local
+    list = TimezonePeriodList.new
+    list.add(2000, 4)  { TimezonePeriod.new(DateTime.new(2000,4,2,1,0,0),DateTime.new(2000,10,2,1,0,0),-18000,3600,:'TESTD') }
+    list.add(2000, 10) { TimezonePeriod.new(DateTime.new(2000,10,2,1,0,0),DateTime.new(2001,3,2,1,0,0),-18000,0,:'TESTS') }    
+    list.add(2001, 3)  { TimezonePeriod.new(DateTime.new(2001,3,2,1,0,0),DateTime.new(2001,4,2,1,0,0),-18000,3600,:'TESTD') }
+    list.add(2001, 4)  { TimezonePeriod.new(DateTime.new(2001,4,2,1,0,0),DateTime.new(2001,10,2,1,0,0),-21600,3600,:'TESTD') }
+    list.add(2001, 10)  { TimezonePeriod.new(DateTime.new(2001,10,2,1,0,0),DateTime.new(2002,10,2,1,0,0),-18000,0,:'TESTS') }
+    list.add(2002, 10)  { TimezonePeriod.new(DateTime.new(2002,10,2,1,0,0),DateTime.new(2003,2,2,1,0,0),-18000,0,:'TESTS') }
+    
+    # TimezonePeriodList expands out blocks and caches the results
+    # run twice to ensure this is being done correctly
+    2.times {|i|
+      assert_raise(PeriodNotFound) {
+        list.periods_for_local(DateTime.new(1999,12,1,0,0,0))
+      }
+      assert_raise(PeriodNotFound) {
+        list.periods_for_local(DateTime.new(2000,4,1,20,59,59))
+      }
+      
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2000,4,2,1,0,0),DateTime.new(2000,10,2,1,0,0),-18000,3600,:'TESTD')],
+        list.periods_for_local(DateTime.new(2000,4,1,21,0,0)))
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2000,4,2,1,0,0),DateTime.new(2000,10,2,1,0,0),-18000,3600,:'TESTD')],
+        list.periods_for_local(DateTime.new(2000,10,1,19,59,59)))
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2000,4,2,1,0,0),DateTime.new(2000,10,2,1,0,0),-18000,3600,:'TESTD'),
+        TimezonePeriod.new(DateTime.new(2000,10,2,1,0,0),DateTime.new(2001,3,2,1,0,0),-18000,0,:'TESTS')],
+        list.periods_for_local(DateTime.new(2000,10,1,20,0,0)))
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2000,4,2,1,0,0),DateTime.new(2000,10,2,1,0,0),-18000,3600,:'TESTD'),
+        TimezonePeriod.new(DateTime.new(2000,10,2,1,0,0),DateTime.new(2001,3,2,1,0,0),-18000,0,:'TESTS')],
+        list.periods_for_local(DateTime.new(2000,10,1,20,59,59)))
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2000,10,2,1,0,0),DateTime.new(2001,3,2,1,0,0),-18000,0,:'TESTS')],
+        list.periods_for_local(DateTime.new(2000,10,1,21,0,0)))
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2000,10,2,1,0,0),DateTime.new(2001,3,2,1,0,0),-18000,0,:'TESTS')],
+        list.periods_for_local(DateTime.new(2001,3,1,19,59,59)))
+        
+      assert_raise(PeriodNotFound) {
+        list.periods_for_local(DateTime.new(2001,3,1,20,0,0))
+      }
+      assert_raise(PeriodNotFound) {
+        list.periods_for_local(DateTime.new(2001,3,1,20,59,59))
+      }
+      
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2001,3,2,1,0,0),DateTime.new(2001,4,2,1,0,0),-18000,3600,:'TESTD')],
+        list.periods_for_local(DateTime.new(2001,3,1,21,0,0)))
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2001,3,2,1,0,0),DateTime.new(2001,4,2,1,0,0),-18000,3600,:'TESTD')],
+        list.periods_for_local(DateTime.new(2001,4,1,19,59,59)))
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2001,3,2,1,0,0),DateTime.new(2001,4,2,1,0,0),-18000,3600,:'TESTD'),
+        TimezonePeriod.new(DateTime.new(2001,4,2,1,0,0),DateTime.new(2001,10,2,1,0,0),-21600,3600,:'TESTD')],
+        list.periods_for_local(DateTime.new(2001,4,1,20,0,0)))
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2001,3,2,1,0,0),DateTime.new(2001,4,2,1,0,0),-18000,3600,:'TESTD'),
+        TimezonePeriod.new(DateTime.new(2001,4,2,1,0,0),DateTime.new(2001,10,2,1,0,0),-21600,3600,:'TESTD')],
+        list.periods_for_local(DateTime.new(2001,4,1,20,59,59)))
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2001,4,2,1,0,0),DateTime.new(2001,10,2,1,0,0),-21600,3600,:'TESTD')],
+        list.periods_for_local(DateTime.new(2001,4,1,21,0,0)))
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2001,4,2,1,0,0),DateTime.new(2001,10,2,1,0,0),-21600,3600,:'TESTD')],
+        list.periods_for_local(DateTime.new(2001,10,1,19,59,59)))
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2001,10,2,1,0,0),DateTime.new(2002,10,2,1,0,0),-18000,0,:'TESTS')],
+        list.periods_for_local(DateTime.new(2001,10,1,20,0,0)))
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2001,10,2,1,0,0),DateTime.new(2002,10,2,1,0,0),-18000,0,:'TESTS')],
+        list.periods_for_local(DateTime.new(2002,2,1,20,0,0)))
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2001,10,2,1,0,0),DateTime.new(2002,10,2,1,0,0),-18000,0,:'TESTS')],
+        list.periods_for_local(DateTime.new(2002,10,1,19,59,59)))
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2002,10,2,1,0,0),DateTime.new(2003,2,2,1,0,0),-18000,0,:'TESTS')],
+        list.periods_for_local(DateTime.new(2002,10,1,20,0,0)))
+      assert_periods_equal([TimezonePeriod.new(DateTime.new(2002,10,2,1,0,0),DateTime.new(2003,2,2,1,0,0),-18000,0,:'TESTS')],
+        list.periods_for_local(DateTime.new(2003,2,1,19,59,59)))
+        
+      assert_raise(PeriodNotFound) {
+        list.periods_for_local(DateTime.new(2003,2,1,20,0,0))
+      }
+      assert_raise(PeriodNotFound) {
+        list.periods_for_local(DateTime.new(2004,1,1,20,0,0))
+      }  
+    }
   end
   
-  def test_periods_for_local_dense
-    # test with more than one start in a six month period
+  def test_period_for_local_unbounded
+    list = TimezonePeriodList.new
+    list.add_unbounded_start  { TimezonePeriod.new(nil,DateTime.new(2000,10,2,1,0,0),-18000,3600,:'TESTD') }
+    list.add(2000, 10) { TimezonePeriod.new(DateTime.new(2000,10,2,1,0,0),DateTime.new(2001,3,2,1,0,0),-18000,0,:'TESTS') }    
+    list.add(2001, 3)  { TimezonePeriod.new(DateTime.new(2001,3,2,1,0,0),nil,-18000,3600,:'TESTD') }
+        
+    assert_periods_equal([TimezonePeriod.new(nil,DateTime.new(2000,10,2,1,0,0),-18000,3600,:'TESTD')],
+      list.periods_for_local(DateTime.new(1999,12,1,1,0,0)))
+    assert_periods_equal([TimezonePeriod.new(nil,DateTime.new(2000,10,2,1,0,0),-18000,3600,:'TESTD')],
+      list.periods_for_local(DateTime.new(2000,10,1,20,59,59)))
+    assert_periods_equal([TimezonePeriod.new(nil,DateTime.new(2000,10,2,1,0,0),-18000,3600,:'TESTD')],
+      list.periods_for_local(DateTime.new(2000,10,1,19,59,59)))
+    assert_periods_equal([TimezonePeriod.new(nil,DateTime.new(2000,10,2,1,0,0),-18000,3600,:'TESTD'),
+      TimezonePeriod.new(DateTime.new(2000,10,2,1,0,0),DateTime.new(2001,3,2,1,0,0),-18000,0,:'TESTS')],
+      list.periods_for_local(DateTime.new(2000,10,1,20,0,0)))
+    assert_periods_equal([TimezonePeriod.new(nil,DateTime.new(2000,10,2,1,0,0),-18000,3600,:'TESTD'),
+      TimezonePeriod.new(DateTime.new(2000,10,2,1,0,0),DateTime.new(2001,3,2,1,0,0),-18000,0,:'TESTS')],
+      list.periods_for_local(DateTime.new(2000,10,1,20,59,59)))       
+    assert_periods_equal([TimezonePeriod.new(DateTime.new(2000,10,2,1,0,0),DateTime.new(2001,3,2,1,0,0),-18000,0,:'TESTS')],
+      list.periods_for_local(DateTime.new(2000,10,1,21,0,0)))
+    assert_periods_equal([TimezonePeriod.new(DateTime.new(2000,10,2,1,0,0),DateTime.new(2001,3,2,1,0,0),-18000,0,:'TESTS')],
+      list.periods_for_local(DateTime.new(2001,3,1,19,59,59)))
+    assert_periods_equal([TimezonePeriod.new(DateTime.new(2000,10,2,1,0,0),DateTime.new(2001,3,2,1,0,0),-18000,0,:'TESTS')],
+      list.periods_for_local(DateTime.new(2001,3,1,19,59,59)))
+      
+    assert_raise(PeriodNotFound) {
+      list.periods_for_local(DateTime.new(2001,3,1,20,0,0))
+    }
+    assert_raise(PeriodNotFound) {
+      list.periods_for_local(DateTime.new(2001,3,1,20,59,59))
+    }
+    
+    assert_periods_equal([TimezonePeriod.new(DateTime.new(2001,3,2,1,0,0),nil,-18000,3600,:'TESTD')],
+      list.periods_for_local(DateTime.new(2001,3,1,21,0,0)))
+    assert_periods_equal([TimezonePeriod.new(DateTime.new(2001,3,2,1,0,0),nil,-18000,3600,:'TESTD')],
+      list.periods_for_local(DateTime.new(2002,1,1,21,0,0)))
+  end
+  
+  def test_period_for_local_unbounded
+    list = TimezonePeriodList.new
+    list.add_unbounded_start  { TimezonePeriod.new(nil,DateTime.new(2000,7,1,0,0,0),-3600,0,:'TESTD') }
+    list.add(2000, 7) { TimezonePeriod.new(DateTime.new(2000,7,1,0,0,0),nil,-3600,0,:'TESTS') }    
+    
+    # 2000-07-01 00:00:00 UTC is 2000-06-30 23:00:00 UTC-1
+    # hence to find periods for local times between 2000-06-30 23:00:00
+    # and 2000-07-01 00:00:00 a search has to be carried out in the next half
+    # year to the one containing the date we are looking for
+    
+    assert_periods_equal([TimezonePeriod.new(nil,DateTime.new(2000,7,1,0,0,0),-3600,0,:'TESTD')],
+      list.periods_for_local(DateTime.new(2000,6,30,22,59,59)))
+    assert_periods_equal([TimezonePeriod.new(DateTime.new(2000,7,1,0,0,0),nil,-3600,0,:'TESTS')],
+      list.periods_for_local(DateTime.new(2000,6,30,23,0,0)))
+    assert_periods_equal([TimezonePeriod.new(DateTime.new(2000,7,1,0,0,0),nil,-3600,0,:'TESTS')],
+      list.periods_for_local(DateTime.new(2000,7,1,0,0,0)))
   end
   
   private
@@ -71,5 +277,12 @@ class TCTimezonePeriod < Test::Unit::TestCase
       assert_equal(expected.zone_identifier, actual.zone_identifier)    
       assert_equal(expected.local_start, actual.local_start)    
       assert_equal(expected.local_end, actual.local_end)
+    end
+    
+    def assert_periods_equal(expected, actual)     
+      assert_equal(expected.length, actual.length)
+      (0..expected.length - 1).each {|i|
+        assert_period_equal(expected[i], actual[i])
+      }
     end
 end
