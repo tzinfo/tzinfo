a910b83184e6cac3087138effd4082eace1b7cf1
Phil Ross <phil.ross@gmail.com>
2006-01-04 20:41:40 +0000

    Add lib directory to the load path when tzinfo is required. Makes it easier to use tzinfo gem when unpacked to vendor directory in rails.

--- tzinfo/lib/tzinfo.rb
+++ tzinfo/lib/tzinfo.rb
@@ -20,6 +20,11 @@
 # THE SOFTWARE.
 #++
 
+# Add the directory containing this file to the start of the load path if it
+# isn't there already.
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
 require 'tzinfo/timezone'
 require 'tzinfo/country'
 require 'tzinfo/tzdataparser'
