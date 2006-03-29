20c6ecd17a30285b1ed38701459f5ae382112924
Phil Ross <phil.ross@gmail.com>
2006-03-29 13:01:33 +0000

    Script to test all zones against zdump.

--- utils/zdumptest.rb
+++ utils/zdumptest.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/ruby
+
 # Reads zdump formatted lines from stdin. Uses the UTC time in the zdump line
 # to calculate the local time and zone identifier using TZInfo. If there is
 # a difference, a message is output to stdout.
@@ -8,16 +10,21 @@ include TZInfo
 
 STDIN.each {|line|
  if line =~ /^([^\s]+)\s+([A-z][a-z]{2}\s[A-z][a-z]{2}\s+[0-9]+\s[0-9]+:[0-9]+:[0-9]+\s[0-9]+)\s[A-Z]+\s=\s([A-z][a-z]{2}\s[A-z][a-z]{2}\s+[0-9]+\s[0-9]+:[0-9]+:[0-9]+\s[0-9]+)\s([A-Z]+)/ then
-   zone = Timezone.get($1)
-   utc = DateTime.parse($2)
-   local = DateTime.parse($3)
-   identifier = $4
+   begin
+     zone = Timezone.get($1)
+     utc = DateTime.parse($2)
+     local = DateTime.parse($3)
+     identifier = $4
    
-   tzi_local = zone.utc_to_local(utc)
-   tzi_identifier = zone.period_for_utc(utc).zone_identifier
+     tzi_local = zone.utc_to_local(utc)
+     tzi_identifier = zone.period_for_utc(utc).zone_identifier
    
-   if tzi_local != local || tzi_identifier.to_s != identifier
-     puts "At #{utc} UTC zdump returns #{local} #{identifier}, tzinfo returns #{tzi_local} #{tzi_identifier}"     
+     if tzi_local != local || tzi_identifier.to_s != identifier
+       puts "zdump:  #{zone.identifier} #{utc} UTC #{local} #{identifier}"
+       puts "tzinfo: #{zone.identifier} #{utc} UTC #{tzi_local} #{tzi_identifier}"
+     end
+   rescue InvalidTimezoneIdentifier
+     puts "Unrecognized timezone: #$1"
    end
  end
 }
---
+++ utils/zdumptestall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+  echo "Usage: $0 <zoneinfoDir> <zdumpCommand>"
+fi
+
+zoneinfo=$1
+zdump=$2
+
+pushd $zoneinfo > /dev/null
+find . ! -name localtime ! -name posixrules ! -name '*.tab' -type f -print | sed -e 's/^\.\///' | sort |
+{
+  popd > /dev/null
+
+  while read zone
+  do
+    echo "Testing zone $zone"
+    $zdump -v $zone | ./zdumptest.rb
+  done
+}
