deca06cfe37fb0e68ce7cf4d0d4ba04583ce1346
Phil Ross <phil.ross@gmail.com>
2006-05-17 22:03:44 +0000

    Updated to tzdata version 2006g.

--- tzinfo/lib/tzinfo/definitions/America/Guatemala.rb
+++ tzinfo/lib/tzinfo/definitions/America/Guatemala.rb
@@ -12,7 +12,9 @@ add_period(1974,2) {TimezonePeriod.new(130914000,422344800,-21600,0,:CST)}
 add_period(1983,5) {TimezonePeriod.new(422344800,433054800,-21600,3600,:CDT)}
 add_period(1983,9) {TimezonePeriod.new(433054800,669708000,-21600,0,:CST)}
 add_period(1991,3) {TimezonePeriod.new(669708000,684219600,-21600,3600,:CDT)}
-add_period(1991,9) {TimezonePeriod.new(684219600,nil,-21600,0,:CST)}
+add_period(1991,9) {TimezonePeriod.new(684219600,1146376800,-21600,0,:CST)}
+add_period(2006,4) {TimezonePeriod.new(1146376800,1159678800,-21600,3600,:CDT)}
+add_period(2006,10) {TimezonePeriod.new(1159678800,nil,-21600,0,:CST)}
 end
 end
 end
