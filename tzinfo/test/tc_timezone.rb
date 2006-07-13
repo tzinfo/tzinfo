bdcd28174519f7b675b1df53a650269c024ae390
Phil Ross <phil.ross@gmail.com>
2006-07-13 23:22:46 +0000

    Additional assertions for timezone equality tests.

--- tzinfo/test/tc_timezone.rb
+++ tzinfo/test/tc_timezone.rb
@@ -755,13 +755,15 @@ class TCTimezone < Test::Unit::TestCase
   def test_equality
     assert_equal(true, TestTimezone.new('Europe/London') == TestTimezone.new('Europe/London'))
     assert_not_equal(true, TestTimezone.new('Europe/London') == TestTimezone.new('Europe/london'))
-    assert_not_equal(true, TestTimezone.new('Europe/London') == TestTimezone.new('Europe/Paris'))   
+    assert_not_equal(true, TestTimezone.new('Europe/London') == TestTimezone.new('Europe/Paris'))
+    assert_not_equal(true, TestTimezone.new('Europe/London') == Object.new)
   end
   
   def test_eql
     assert_equal(true, TestTimezone.new('Europe/London').eql?(TestTimezone.new('Europe/London')))
     assert_not_equal(true, TestTimezone.new('Europe/London').eql?(TestTimezone.new('Europe/london')))
-    assert_not_equal(true, TestTimezone.new('Europe/London').eql?(TestTimezone.new('Europe/Paris')))   
+    assert_not_equal(true, TestTimezone.new('Europe/London').eql?(TestTimezone.new('Europe/Paris')))
+    assert_not_equal(true, TestTimezone.new('Europe/London') == Object.new)
   end
   
   def test_hash
