4c2701730f983e9b763eb83e00c07f55e7b8aa97
Phil Ross <phil.ross@gmail.com>
2011-06-12 00:32:53 +0000

    Merge revision 314 from 0.3 branch. Since Ruby revision 31862, DateTime.jd no longer accepts a fractional Julian Day number if any of the subsequent parameters are specified. Since setting the offset and calendar reform date is supported by RubyCoreSupport.datetime_new!, the hours, minutes and seconds must now be calculated from the fractional part of the Julian Day number.

--- tzinfo/test/tc_ruby_core_support.rb
+++ tzinfo/test/tc_ruby_core_support.rb
@@ -10,6 +10,12 @@ class TCRubyCoreSupport < Test::Unit::TestCase
   end
   
   def test_datetime_new!
+    assert_equal(DateTime.new(2008,10,5,12,0,0, 0, Date::ITALY), RubyCoreSupport.datetime_new!(2454745,0,2299161))
+    assert_equal(DateTime.new(2008,10,5,20,30,0, 0, Date::ITALY), RubyCoreSupport.datetime_new!(Rational(117827777, 48),0,2299161))
+    assert_equal(DateTime.new(2008,10,6,6,26,21, 0, Date::ITALY), RubyCoreSupport.datetime_new!(Rational(70696678127,28800),0,2299161))
+    
+    assert_equal(DateTime.new(2008,10,6,12,0,0, 1, Date::ITALY), RubyCoreSupport.datetime_new!(2454745,1,2299161))
     assert_equal(DateTime.new(2008,10,6,20,30,0, 1, Date::ITALY), RubyCoreSupport.datetime_new!(Rational(117827777, 48),1,2299161))
+    assert_equal(DateTime.new(2008,10,7,6,26,21, 1, Date::ITALY), RubyCoreSupport.datetime_new!(Rational(70696678127, 28800),1,2299161))
   end
 end
