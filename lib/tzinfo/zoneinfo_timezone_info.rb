30b71df80c82cc32257f2771c8c4cae4df9de65c
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2016-09-20 19:24:28 -0500

    use a more concise syntax and avoid unnecessary objects
    (cherry picked from commit ba72d8eeb112f09293e8e1ca5c5675ce7fc7ac0c)

--- lib/tzinfo/zoneinfo_timezone_info.rb
+++ lib/tzinfo/zoneinfo_timezone_info.rb
@@ -197,26 +197,26 @@ module TZInfo
         transitions = []
         
         if using_64bit
-          (0...timecnt).each do |i|
+          timecnt.times do |i|
             high, low = check_read(file, 8).unpack('NN'.freeze)
             transition_time = make_signed_int64(high, low)
             transitions << {:at => transition_time}          
           end
         else
-          (0...timecnt).each do |i|
+          timecnt.times do |i|
             transition_time = make_signed_int32(check_read(file, 4).unpack('N'.freeze)[0])
             transitions << {:at => transition_time}          
           end
         end
         
-        (0...timecnt).each do |i|
+        timecnt.times do |i|
           localtime_type = check_read(file, 1).unpack('C'.freeze)[0]
           transitions[i][:offset] = localtime_type
         end
         
         offsets = []
         
-        (0...typecnt).each do |i|
+        typecnt.times do |i|
           gmtoff, isdst, abbrind = check_read(file, 6).unpack('NCC'.freeze)
           gmtoff = make_signed_int32(gmtoff)
           isdst = isdst == 1
