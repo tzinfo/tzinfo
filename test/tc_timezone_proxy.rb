9a8c076b29f25c79feeb7a05372b0d16eaa803a7
Phil Ross <phil.ross@gmail.com>
2017-01-04 23:44:46 +0000

    Fix TimezoneProxy UnknownTimezone exception.
    When calling transitions_up_to or offsets_up_to on a TimezoneProxy
    instance, an UnknownTimezone exception would be raised. Implement
    TimezoneProxy#transitions_up_to so that both methods now return the
    correct result.
    
    Expand TimezoneProxy tests to cover all Timezone instance methods.

--- test/tc_timezone_proxy.rb
+++ test/tc_timezone_proxy.rb
@@ -10,44 +10,62 @@ class TCTimezoneProxy < Minitest::Test
 
   def test_not_exist
     proxy = TimezoneProxy.new('Nothing/Special')
+    t = Time.utc(2006,1,1,0,0,0)
     assert_equal('Nothing/Special', proxy.identifier)
-    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.now }
+    assert_equal('Nothing/Special', proxy.name)
+    assert_equal('Nothing - Special', proxy.friendly_identifier)
+    assert_equal('Nothing - Special', proxy.to_s)
+
+    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.canonical_identifier }
+    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.canonical_zone }
     assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.current_period }
     assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.current_period_and_time }
     assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.current_time_and_period }
-    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.utc_to_local(DateTime.new(2006,1,1,0,0,0)) }
-    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.local_to_utc(DateTime.new(2006,1,1,0,0,0)) }
-    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.period_for_utc(DateTime.new(2006,1,1,0,0,0)) }
-    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.period_for_local(DateTime.new(2006,1,1,0,0,0)) }
-    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.canonical_identifier }
-    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.canonical_zone }
+    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.local_to_utc(t) }
+    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.now }
+    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.offsets_up_to(t) }
+    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.period_for(t) }
+    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.period_for_local(t) }
+    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.period_for_utc(t) }
+    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.periods_for_local(t) }
+    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.strftime('%Z', t) }
+    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.to_local(t) }
+    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.transitions_up_to(t) }
+    assert_raises_invalid_timezone_identifier('Nothing/Special') { proxy.utc_to_local(t) }
   end
 
   def test_valid
     proxy = TimezoneProxy.new('Europe/London')
-    assert_equal('Europe/London', proxy.identifier)
+    real = Timezone.get('Europe/London')
 
-    assert_nothing_raised { proxy.now }
+    t1 = Time.utc(2005,8,1,0,0,0)
+    t2 = Time.utc(2004,8,1,0,0,0)
+
+    assert_equal(real.canonical_identifier, proxy.canonical_identifier)
+    assert_same(real.canonical_zone, proxy.canonical_zone)
     assert_nothing_raised { proxy.current_period }
     assert_nothing_raised { proxy.current_period_and_time }
     assert_nothing_raised { proxy.current_time_and_period }
-
-    real = Timezone.get('Europe/London')
-
-    assert_equal(real.utc_to_local(DateTime.new(2005,8,1,0,0,0)), proxy.utc_to_local(DateTime.new(2005,8,1,0,0,0)))
-    assert_equal(real.local_to_utc(DateTime.new(2005,8,1,0,0,0)), proxy.local_to_utc(DateTime.new(2005,8,1,0,0,0)))
-    assert_equal(real.period_for_utc(DateTime.new(2005,8,1,0,0,0)), proxy.period_for_utc(DateTime.new(2005,8,1,0,0,0)))
-    assert_equal(real.period_for_local(DateTime.new(2005,8,1,0,0,0)), proxy.period_for_local(DateTime.new(2005,8,1,0,0,0)))
-    assert_equal(real.identifier, proxy.identifier)
-    assert_equal(real.name, proxy.name)
-    assert_equal(real.to_s, proxy.to_s)
     assert_equal(real.friendly_identifier(true), proxy.friendly_identifier(true))
     assert_equal(real.friendly_identifier(false), proxy.friendly_identifier(false))
     assert_equal(real.friendly_identifier, proxy.friendly_identifier)
-    assert_equal(real.canonical_identifier, proxy.canonical_identifier)
-    assert_same(real.canonical_zone, proxy.canonical_zone)
+    assert_equal(real.identifier, proxy.identifier)
+    assert_equal_with_offset(real.local_to_utc(t1), proxy.local_to_utc(t1))
+    assert_equal(real.name, proxy.name)
+    assert_nothing_raised { proxy.now }
+    assert_equal(real.offsets_up_to(t1), proxy.offsets_up_to(t1))
+    assert_equal(real.offsets_up_to(t1, t2), proxy.offsets_up_to(t1, t2))
+    assert_equal(real.period_for(t1), proxy.period_for(t1))
+    assert_equal(real.period_for_local(t1), proxy.period_for_local(t1))
+    assert_equal(real.period_for_utc(t1), proxy.period_for_utc(t1))
+    assert_equal(real.periods_for_local(t1), proxy.periods_for_local(t1))
+    assert_equal(real.strftime('%Z', t1), proxy.strftime('%Z', t1))
+    assert_equal_with_offset(real.to_local(t1), proxy.to_local(t1))
+    assert_equal(real.to_s, proxy.to_s)
+    assert_equal(real.transitions_up_to(t1), proxy.transitions_up_to(t1))
+    assert_equal(real.transitions_up_to(t1, t2), proxy.transitions_up_to(t1, t2))
+    assert_equal_with_offset(real.utc_to_local(t1), proxy.utc_to_local(t1))
 
-    assert_equal('Europe/London', proxy.identifier)
 
     assert(real == proxy)
     assert(proxy == real)
