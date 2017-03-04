d3a17b35461905e60de499733e5de30e1a77a27f
Phil Ross <phil.ross@gmail.com>
2017-03-04 20:25:04 +0000

    Create test modules dynamically.

--- test/tc_timezone_index_definition.rb
+++ test/tc_timezone_index_definition.rb
@@ -3,116 +3,104 @@ require File.join(File.expand_path(File.dirname(__FILE__)), 'test_utils')
 include TZInfo
 
 class TCTimezoneIndexDefinition < Minitest::Test
+  def test_mixed
+    m = Module.new
+    m.send(:include, TimezoneIndexDefinition)
 
-  module TimezonesTest1
-    include TimezoneIndexDefinition
+    m.send(:timezone, 'Test/One')
+    m.send(:timezone, 'Test/Two')
+    m.send(:linked_timezone, 'Test/Three')
+    m.send(:timezone, 'Another/Zone')
+    m.send(:linked_timezone, 'And/Yet/Another')
 
-    timezone 'Test/One'
-    timezone 'Test/Two'
-    linked_timezone 'Test/Three'
-    timezone 'Another/Zone'
-    linked_timezone 'And/Yet/Another'
+    timezones = m.timezones
+    assert_equal(['And/Yet/Another', 'Another/Zone', 'Test/One', 'Test/Three', 'Test/Two'], timezones)
+    assert(timezones.frozen?)
+    assert(timezones.all?(&:frozen?))
+    assert_same(timezones, m.timezones)
+
+    data_timezones = m.data_timezones
+    assert_equal(['Another/Zone', 'Test/One', 'Test/Two'], data_timezones)
+    assert(data_timezones.frozen?)
+    assert(data_timezones.all?(&:frozen?))
+    assert_same(data_timezones, m.data_timezones)
+
+    linked_timezones = m.linked_timezones
+    assert_equal(['And/Yet/Another', 'Test/Three'], linked_timezones)
+    assert(linked_timezones.frozen?)
+    assert(linked_timezones.all?(&:frozen?))
+    assert_same(linked_timezones, m.linked_timezones)
   end
 
-  module TimezonesTest2
-    include TimezoneIndexDefinition
+  def test_data_only
+    m = Module.new
+    m.send(:include, TimezoneIndexDefinition)
 
-    timezone 'Test/A/One'
-    timezone 'Test/A/Two'
-    timezone 'Test/A/Three'
+    m.send(:timezone, 'Test/A/One')
+    m.send(:timezone, 'Test/A/Two')
+    m.send(:timezone, 'Test/A/Three')
+
+    timezones = m.timezones
+    assert_equal(['Test/A/One', 'Test/A/Three', 'Test/A/Two'], timezones)
+    assert(timezones.frozen?)
+    assert(timezones.all?(&:frozen?))
+    assert_same(timezones, m.timezones)
+
+    data_timezones = m.data_timezones
+    assert_equal(['Test/A/One', 'Test/A/Three', 'Test/A/Two'], data_timezones)
+    assert(data_timezones.frozen?)
+    assert(data_timezones.all?(&:frozen?))
+    assert_same(data_timezones, m.data_timezones)
+
+    linked_timezones = m.linked_timezones
+    assert_equal([], linked_timezones)
+    assert(linked_timezones.frozen?)
+    assert_same(linked_timezones, m.linked_timezones)
   end
 
-  module TimezonesTest3
-    include TimezoneIndexDefinition
+  def test_linked_only
+    m = Module.new
+    m.send(:include, TimezoneIndexDefinition)
 
-    linked_timezone 'Test/B/One'
-    linked_timezone 'Test/B/Two'
-    linked_timezone 'Test/B/Three'
+    m.send(:linked_timezone, 'Test/B/One')
+    m.send(:linked_timezone, 'Test/B/Two')
+    m.send(:linked_timezone, 'Test/B/Three')
+
+    timezones = m.timezones
+    assert_equal(['Test/B/One', 'Test/B/Three', 'Test/B/Two'], timezones)
+    assert(timezones.frozen?)
+    assert(timezones.all?(&:frozen?))
+    assert_same(timezones, m.timezones)
+
+    data_timezones = m.data_timezones
+    assert_equal([], data_timezones)
+    assert(data_timezones.frozen?)
+    assert_same(data_timezones, m.data_timezones)
+
+    linked_timezones = m.linked_timezones
+    assert_equal(['Test/B/One', 'Test/B/Three', 'Test/B/Two'], linked_timezones)
+    assert(linked_timezones.frozen?)
+    assert(linked_timezones.all?(&:frozen?))
+    assert_same(linked_timezones, m.linked_timezones)
   end
 
-  module TimezonesTest4
-    include TimezoneIndexDefinition
+  def test_none
+    m = Module.new
+    m.send(:include, TimezoneIndexDefinition)
 
-  end
+    timezones = m.timezones
+    assert_equal([], timezones)
+    assert(timezones.frozen?)
+    assert_same(timezones, m.timezones)
 
-  def test_timezones
-    timezones1 = TimezonesTest1.timezones
-    timezones2 = TimezonesTest2.timezones
-    timezones3 = TimezonesTest3.timezones
-    timezones4 = TimezonesTest4.timezones
+    data_timezones = m.data_timezones
+    assert_equal([], data_timezones)
+    assert(data_timezones.frozen?)
+    assert_same(data_timezones, m.data_timezones)
 
-    assert_equal(['And/Yet/Another', 'Another/Zone', 'Test/One', 'Test/Three', 'Test/Two'], timezones1)
-    assert_equal(['Test/A/One', 'Test/A/Three', 'Test/A/Two'], timezones2)
-    assert_equal(['Test/B/One', 'Test/B/Three', 'Test/B/Two'], timezones3)
-    assert_equal([], timezones4)
-
-    assert(timezones1.frozen?)
-    assert(timezones2.frozen?)
-    assert(timezones3.frozen?)
-    assert(timezones4.frozen?)
-
-    assert(timezones1.all?(&:frozen?))
-    assert(timezones2.all?(&:frozen?))
-    assert(timezones3.all?(&:frozen?))
-    assert(timezones4.all?(&:frozen?))
-
-    assert_same(timezones1, TimezonesTest1.timezones)
-    assert_same(timezones2, TimezonesTest2.timezones)
-    assert_same(timezones3, TimezonesTest3.timezones)
-    assert_same(timezones4, TimezonesTest4.timezones)
-  end
-
-  def test_data_timezones
-    data_timezones1 = TimezonesTest1.data_timezones
-    data_timezones2 = TimezonesTest2.data_timezones
-    data_timezones3 = TimezonesTest3.data_timezones
-    data_timezones4 = TimezonesTest4.data_timezones
-
-    assert_equal(['Another/Zone', 'Test/One', 'Test/Two'], data_timezones1)
-    assert_equal(['Test/A/One', 'Test/A/Three', 'Test/A/Two'], data_timezones2)
-    assert_equal([], data_timezones3)
-    assert_equal([], data_timezones4)
-
-    assert(data_timezones1.frozen?)
-    assert(data_timezones2.frozen?)
-    assert(data_timezones3.frozen?)
-    assert(data_timezones4.frozen?)
-
-    assert(data_timezones1.all?(&:frozen?))
-    assert(data_timezones2.all?(&:frozen?))
-    assert(data_timezones3.all?(&:frozen?))
-    assert(data_timezones4.all?(&:frozen?))
-
-    assert_same(data_timezones1, TimezonesTest1.data_timezones)
-    assert_same(data_timezones2, TimezonesTest2.data_timezones)
-    assert_same(data_timezones3, TimezonesTest3.data_timezones)
-    assert_same(data_timezones4, TimezonesTest4.data_timezones)
-  end
-
-  def test_linked_timezones
-    linked_timezones1 = TimezonesTest1.linked_timezones
-    linked_timezones2 = TimezonesTest2.linked_timezones
-    linked_timezones3 = TimezonesTest3.linked_timezones
-    linked_timezones4 = TimezonesTest4.linked_timezones
-
-    assert_equal(['And/Yet/Another', 'Test/Three'], linked_timezones1)
-    assert_equal([], linked_timezones2)
-    assert_equal(['Test/B/One', 'Test/B/Three', 'Test/B/Two'], linked_timezones3)
-    assert_equal([], linked_timezones4)
-
-    assert(linked_timezones1.frozen?)
-    assert(linked_timezones2.frozen?)
-    assert(linked_timezones3.frozen?)
-    assert(linked_timezones4.frozen?)
-
-    assert(linked_timezones1.all?(&:frozen?))
-    assert(linked_timezones2.all?(&:frozen?))
-    assert(linked_timezones3.all?(&:frozen?))
-    assert(linked_timezones4.all?(&:frozen?))
-
-    assert_same(linked_timezones1, TimezonesTest1.linked_timezones)
-    assert_same(linked_timezones2, TimezonesTest2.linked_timezones)
-    assert_same(linked_timezones3, TimezonesTest3.linked_timezones)
-    assert_same(linked_timezones4, TimezonesTest4.linked_timezones)
+    linked_timezones = m.linked_timezones
+    assert_equal([], linked_timezones)
+    assert(linked_timezones.frozen?)
+    assert_same(linked_timezones, m.linked_timezones)
   end
 end
