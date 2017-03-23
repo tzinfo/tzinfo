4a13705a97ed52813b48f7f531ee3bbf3eb82360
Phil Ross <phil.ross@gmail.com>
2017-03-23 20:32:44 +0000

    Update to tzdata version 2017b.
    https://mm.icann.org/pipermail/tz-announce/2017-March/000046.html

--- lib/tzinfo/definitions/Africa/Monrovia.rb
+++ lib/tzinfo/definitions/Africa/Monrovia.rb
@@ -9,12 +9,12 @@ module TZInfo
         timezone 'Africa/Monrovia' do |tz|
           tz.offset :o0, -2588, 0, :LMT
           tz.offset :o1, -2588, 0, :MMT
-          tz.offset :o2, -2670, 0, :'-004430'
+          tz.offset :o2, -2670, 0, :MMT
           tz.offset :o3, 0, 0, :GMT
           
           tz.transition 1882, 1, :o1, 52022445047, 21600
           tz.transition 1919, 3, :o2, 52315600247, 21600
-          tz.transition 1972, 5, :o3, 73529070
+          tz.transition 1972, 1, :o3, 63593070
         end
       end
     end
