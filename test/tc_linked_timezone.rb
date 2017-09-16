b3b226b3f3ea6bdde6c29ced0e7ca96440f3a4e8
Phil Ross <phil.ross@gmail.com>
2017-09-16 22:39:55 +0100

    Use a Minitest stub for Timezone.get instead of reloading timezone.rb.
    The reload resets SimpleCov's coverage tracking, resulting in random
    results (since tests are run in a random order).

--- test/tc_linked_timezone.rb
+++ test/tc_linked_timezone.rb
@@ -54,33 +54,21 @@ class TCLinkedTimezone < Minitest::Test
   end
 
 
-  def setup
-    # Redefine Timezone.get to return a fake timezone.
-    # Use without_warnings to suppress redefined get method warning.
-    without_warnings do
-      def Timezone.get(identifier)
-        raise InvalidTimezoneIdentifier, 'Invalid identifier' if identifier == 'Invalid/Identifier'
-
-        @timezones ||= {}
-        @timezones[identifier] ||=
-          identifier == 'Test/Recursive/Linked' ?
-            LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new(identifier, 'Test/Recursive/Data')) :
-            TestTimezone.new(identifier, identifier == 'Test/No/Local')
-      end
-    end
+  def get_timezone(linked_tz, identifier)
+    assert_equal(linked_tz.identifier, identifier)
+    linked_tz
   end
 
-  def teardown
-    # Re-require timezone to reset.
-    # Suppress redefined method warnings.
-    without_warnings do
-      load 'tzinfo/timezone.rb'
-    end
+  def stub_timezone_get(linked_tz = nil)
+    linked_tz = TestTimezone.new('Test/Linked') unless linked_tz
+    Timezone.stub(:get, ->(i) { get_timezone(linked_tz, i) }) { yield linked_tz }
   end
 
   def test_identifier
-    tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
-    assert_equal('Test/Zone', tz.identifier)
+    stub_timezone_get do
+      tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
+      assert_equal('Test/Zone', tz.identifier)
+    end
   end
 
   def test_invalid_linked_identifier
@@ -88,101 +76,128 @@ class TCLinkedTimezone < Minitest::Test
   end
 
   def test_period_for
-    time_types_test do |h|
-      tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
-      linked_tz = Timezone.get('Test/Linked')
-      t = h.time(2006, 6, 27, 23, 12, 28, Rational(1, 10), 0)
-      assert_same(linked_tz.period_for_result, tz.period_for(t))
-      assert_same(t, linked_tz.time)
+    stub_timezone_get do |linked_tz|
+      time_types_test do |h|
+        tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
+        t = h.time(2006, 6, 27, 23, 12, 28, Rational(1, 10), 0)
+        assert_same(linked_tz.period_for_result, tz.period_for(t))
+        assert_same(t, linked_tz.time)
+      end
     end
   end
 
   def test_period_for_nil
-    tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
+    stub_timezone_get do
+      tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
 
-    error = assert_raises(ArgumentError) { tz.period_for(nil) }
-    assert_equal('linked zone exception: nil', error.message)
+      error = assert_raises(ArgumentError) { tz.period_for(nil) }
+      assert_equal('linked zone exception: nil', error.message)
+    end
   end
 
   def test_periods_for_local
-    time_types_test do |h|
-      tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
-      linked_tz = Timezone.get('Test/Linked')
-      t = h.time(2006, 6, 27, 23, 12, 28, Rational(1, 10), 0)
+    stub_timezone_get do |linked_tz|
+      time_types_test do |h|
+        tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
+        t = h.time(2006, 6, 27, 23, 12, 28, Rational(1, 10), 0)
+        assert_same(linked_tz.periods_for_local_result, tz.periods_for_local(t))
+        assert_same(t, linked_tz.local_time)
+      end
+    end
+  end
+
+  def test_periods_for_local_not_found
+    stub_timezone_get(TestTimezone.new('Test/No/Local', true)) do |linked_tz|
+      tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/No/Local'))
+      t = Time.utc(2006, 6, 27, 23, 12, 28)
+      assert_equal([], linked_tz.periods_for_local_result)
       assert_same(linked_tz.periods_for_local_result, tz.periods_for_local(t))
       assert_same(t, linked_tz.local_time)
     end
   end
 
-  def test_periods_for_local_not_found
-    tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/No/Local'))
-    linked_tz = Timezone.get('Test/No/Local')
-    t = Time.utc(2006, 6, 27, 23, 12, 28)
-    assert_equal([], linked_tz.periods_for_local_result)
-    assert_same(linked_tz.periods_for_local_result, tz.periods_for_local(t))
-    assert_same(t, linked_tz.local_time)
-  end
-
   def test_periods_for_local_nil
-    tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
+    stub_timezone_get do
+      tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
 
-    error = assert_raises(ArgumentError) { tz.periods_for_local(nil) }
-    assert_equal('linked zone exception: nil', error.message)
+      error = assert_raises(ArgumentError) { tz.periods_for_local(nil) }
+      assert_equal('linked zone exception: nil', error.message)
+    end
   end
 
   def test_transitions_up_to
-    time_types_test do |h|
-      tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
-      linked_tz = Timezone.get('Test/Linked')
-      to = h.time(2013, 1, 1, 0, 0, 0, Rational(1, 10), 0)
-      from = h.time(2012, 1, 1, 0, 0, 0, Rational(1, 10), 0)
-      assert_same(linked_tz.transitions_up_to_result, tz.transitions_up_to(to, from))
-      assert_same(to, linked_tz.to)
-      assert_same(from, linked_tz.from)
+    stub_timezone_get do |linked_tz|
+      time_types_test do |h|
+        tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
+        to = h.time(2013, 1, 1, 0, 0, 0, Rational(1, 10), 0)
+        from = h.time(2012, 1, 1, 0, 0, 0, Rational(1, 10), 0)
+        assert_same(linked_tz.transitions_up_to_result, tz.transitions_up_to(to, from))
+        assert_same(to, linked_tz.to)
+        assert_same(from, linked_tz.from)
+      end
     end
   end
 
   def test_transitions_up_to_nil_from
-    time_types_test do |h|
-      tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
-      linked_tz = Timezone.get('Test/Linked')
-      to = h.time(2013, 1, 1, 0, 0, 0, Rational(1, 10), 0)
-      assert_same(linked_tz.transitions_up_to_result, tz.transitions_up_to(to))
-      assert_same(to, linked_tz.to)
-      assert_nil(linked_tz.from)
+    stub_timezone_get do |linked_tz|
+      time_types_test do |h|
+        tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
+        linked_tz = Timezone.get('Test/Linked')
+        to = h.time(2013, 1, 1, 0, 0, 0, Rational(1, 10), 0)
+        assert_same(linked_tz.transitions_up_to_result, tz.transitions_up_to(to))
+        assert_same(to, linked_tz.to)
+        assert_nil(linked_tz.from)
+      end
     end
   end
 
   def test_canonical_identifier
-    tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
-    assert_equal('Test/Linked', tz.canonical_identifier)
+    stub_timezone_get do
+      tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
+      assert_equal('Test/Linked', tz.canonical_identifier)
+    end
   end
 
   def test_canonical_identifier_recursive
     # Recursive links are not currently used in the Time Zone database, but
     # will be supported by TZInfo.
+    recursive_data = TestTimezone.new('Test/Recursive/Data')
+    recursive_linked = stub_timezone_get(recursive_data) do
+      LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Recursive/Linked', 'Test/Recursive/Data'))
+    end
 
-    tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Recursive/Linked'))
-    assert_equal('Test/Recursive/Data', tz.canonical_identifier)
+    stub_timezone_get(recursive_linked) do
+      tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Recursive/Linked'))
+      assert_equal('Test/Recursive/Data', tz.canonical_identifier)
+    end
   end
 
   def test_canonical_zone
-    tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
-    linked_tz = Timezone.get('Test/Linked')
-    assert_same(linked_tz, tz.canonical_zone)
+    stub_timezone_get do |linked_tz|
+      tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
+      assert_same(linked_tz, tz.canonical_zone)
+    end
   end
 
   def test_canonical_zone_recursive
     # Recursive links are not currently used in the Time Zone database, but
     # will be supported by TZInfo.
 
-    tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Recursive/Linked'))
-    linked_tz = Timezone.get('Test/Recursive/Data')
-    assert_same(linked_tz, tz.canonical_zone)
+    recursive_data = TestTimezone.new('Test/Recursive/Data')
+    recursive_linked = stub_timezone_get(recursive_data) do
+      LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Recursive/Linked', 'Test/Recursive/Data'))
+    end
+
+    stub_timezone_get(recursive_linked) do
+      tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Recursive/Linked'))
+      assert_same(recursive_data, tz.canonical_zone)
+    end
   end
 
   def test_inspect
-    tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
-    assert_equal('#<TZInfo::LinkedTimezone: Test/Zone>', tz.inspect)
+    stub_timezone_get do
+      tz = LinkedTimezone.new(DataSources::LinkedTimezoneInfo.new('Test/Zone', 'Test/Linked'))
+      assert_equal('#<TZInfo::LinkedTimezone: Test/Zone>', tz.inspect)
+    end
   end
 end
