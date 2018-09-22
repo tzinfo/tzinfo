98f6dc7e67a9b15c185aaaa6c51bbc85f4b769de
Phil Ross <phil.ross@gmail.com>
2018-09-22 19:38:24 +0100

    Add the (unintentionally omitted) STRING constant.

--- test/tzinfo-data2/tzinfo/data/version.rb
+++ test/tzinfo-data2/tzinfo/data/version.rb
@@ -8,6 +8,9 @@ module TZInfo
       # The format of the Ruby modules. TZInfo v2.0.0 supports formats 1 and 2.
       FORMAT = 2
 
+      # TZInfo::Data version number.
+      STRING = VERSION
+
       # The version of the {IANA Time Zone Database}[http://www.iana.org/time-zones]
       # used to generate this version of TZInfo::Data.
       TZDATA = '2018e'
