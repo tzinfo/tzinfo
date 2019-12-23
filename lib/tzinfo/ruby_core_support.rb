54fce8c1f61ba3b891311f7c7befc32a52ea4a30
Phil Ross <phil.ross@gmail.com>
2019-12-23 17:01:09 +0000

    Eliminate a warning when calling File.open with keyword arguments.

--- lib/tzinfo/ruby_core_support.rb
+++ lib/tzinfo/ruby_core_support.rb
@@ -137,10 +137,19 @@ module TZInfo
       def self.open_file(file_name, mode, opts, &block)
         File.open(file_name, mode, &block)
       end
-    else
+    elsif RUBY_VERSION =~ /\A1\.9\./
       def self.open_file(file_name, mode, opts, &block)
         File.open(file_name, mode, opts, &block)
       end
+    else
+      # Evaluate method as a string because **opts isn't valid syntax prior to
+      # Ruby 2.0.
+      eval(<<-EOF
+        def self.open_file(file_name, mode, opts, &block)
+          File.open(file_name, mode, **opts, &block)
+        end
+      EOF
+      )
     end
 
 
