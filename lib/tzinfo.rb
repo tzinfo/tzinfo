f4e9658bd864c0deb2d6e4255f464d93a683ac7c
Phil Ross <phil.ross@gmail.com>
2020-03-07 20:21:38 +0000

    Test for just the non-existence of #untaint.
    Only Object#untaint is needed and defined by the refinement.

--- lib/tzinfo.rb
+++ lib/tzinfo.rb
@@ -8,8 +8,7 @@ end
 # Object#untaint is a deprecated no-op in Ruby >= 2.7 and will be removed in
 # 3.2. Add a refinement to either silence the warning, or supply the method if
 # needed.
-o = Object.new
-if [:taint, :untaint, :tainted?].none? {|m| o.respond_to?(m) } || RUBY_VERSION =~ /\A(\d+)\.(\d+)(?:\.|\z)/ && ($1 == '2' && $2.to_i >= 7 || $1.to_i >= 3)
+if !Object.new.respond_to?(:untaint) || RUBY_VERSION =~ /\A(\d+)\.(\d+)(?:\.|\z)/ && ($1 == '2' && $2.to_i >= 7 || $1.to_i >= 3)
   require_relative 'tzinfo/untaint_ext'
 end
 
