84333e1c7504d89154791fb3e0e191738a7d410f
Phil Ross <phil.ross@gmail.com>
2021-01-24 22:52:20 +0000

    Update to tzdata version 2021a.
    https://mm.icann.org/pipermail/tz-announce/2021-January/000065.html

--- lib/tzinfo/definitions/Africa/Juba.rb
+++ lib/tzinfo/definitions/Africa/Juba.rb
@@ -46,6 +46,7 @@ module TZInfo
           tz.transition 1985, 4, :o2, 483487200
           tz.transition 1985, 10, :o1, 498171600
           tz.transition 2000, 1, :o3, 947930400
+          tz.transition 2021, 1, :o1, 1612126800
         end
       end
     end
